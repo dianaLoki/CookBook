c974167ab889d8a34a05bdf5738130bf20890f50
Diana Volkova <Volkova6575@gmail.com>
2026-03-23 23:02:02 +0300

    Add profile editing with avatar, bio and media settings

--- cookbook_project/settings.py
+++ cookbook_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,7 +129,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'recipes:index'
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    """Профиль внутри страницы пользователя"""
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Профиль'
+
+
+class CustomUserAdmin(UserAdmin):
+    """Расширенная админка для пользователей с профилем"""
+    inlines = [ProfileInline]
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'get_recipes_count']
+    list_filter = ['is_staff', 'is_superuser', 'is_active', 'groups']
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+
+    def get_recipes_count(self, obj):
+        return obj.recipes.count()
+
+    get_recipes_count.short_description = 'Рецептов'
+
+
+# Переопределяем стандартную админку пользователя
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Отдельная админка для профилей (на всякий случай)"""
+    list_display = ['user', 'avatar_preview', 'bio']
+    list_filter = ['user__is_staff']
+    search_fields = ['user__username', 'bio']
+    readonly_fields = ['avatar_preview']
+
+    def avatar_preview(self, obj):
+        """Показывает миниатюру аватара в админке"""
+        if obj.avatar:
+            from django.utils.html import format_html
+            return format_html('<img src="{}" width="50" height="50" style="border-radius: 50%;" />', obj.avatar.url)
+        return 'Нет аватара'
+
+    avatar_preview.short_description = 'Аватар'
