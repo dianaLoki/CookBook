101b4c112d706aa346cbb690830e25f54229c96b
Diana <Volkova6575@gmail.com>
2026-04-03 23:06:18 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 echo "==> Applying database migrations..."
 python manage.py migrate --noinput
 
+echo "==> Loading fixtures..."
+python manage.py loaddata data.json
+
 echo "==> Starting server..."
 exec gunicorn cookbook_project.wsgi:application --bind 0.0.0.0:$PORT --workers 2
