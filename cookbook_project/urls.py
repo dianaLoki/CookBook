9a367e5b1e25892ba3280113a922cd076f8c6d1b
Diana Volkova <Volkova6575@gmail.com>
2026-04-03 21:13:46 +0300

    Fix database settings: remove dummy DB, use PostgreSQL directly

--- cookbook_project/urls.py
+++ cookbook_project/urls.py
@@ -18,13 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from recipes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipes/', include('recipes.urls')),
     path('users/', include('users.urls')),
+    path('', views.IndexView.as_view(), name='index'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
