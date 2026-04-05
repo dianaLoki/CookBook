9a20bbaaa7d9458bc59b65a26a998a1fbf27520f
Diana <Volkova6575@gmail.com>
2026-04-05 19:14:59 +0300

    Update urls.py

--- cookbook_project/urls.py
+++ cookbook_project/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
